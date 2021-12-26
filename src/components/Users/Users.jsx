e5339cc327f79833dfcefa13dcf5f902d67f32cb
Alex Mercy <hambiru@yandex.ru>
2021-12-26 15:21:13 +0300

    lesson 54 - life cycle methods, componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,23 +5,21 @@ import userPhoto from '../../assets/images/user.jpg'
 
 
 class Users extends React.Component {
-   
-    constructor(props) {
-        super(props);
-            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
             .then(response => {
                 this.props.setUsers(response.data.items)
             });
     }
 
-
     render() {
         return (
-            
+
             <div className={styles.usersPage}>
                 {this.props.users.map(u => <div key={u.id}>
                     <div>
-                        <img src={u.photos.small != null ? u.photos.small: userPhoto } className={styles.photo} />
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.photo} />
                     </div>
                     <div>
                         {u.followed ? <button onClick={() => { this.props.unfollow(u.id) }} >Unfollow</button>
@@ -42,7 +40,7 @@ class Users extends React.Component {
                     <div></div>
                 </div>)}
             </div>
-    
+
         )
     }
 }
