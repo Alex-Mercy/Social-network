9f4cbbf9be8e981d5251f1242dc9ea2029421046
Alex Mercy <hambiru@yandex.ru>
2022-01-03 20:20:56 +0300

    lesson 61 - cookie, login, auth/me

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import './App.css';
-import Header from "./components/Header/Header";
+import HeaderContainer from "./components/Header/HeaderContainer";
 import Navbar from "./components/Navbar/Navbar";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import News from "./components/News/News";
@@ -16,7 +16,7 @@ const App = (props) => {
     return (
         <BrowserRouter>
             <div className='app-wrapper'>
-                <Header />
+                <HeaderContainer />
                 <Navbar />
                 <div className="app-wrapper__content">
                     <Routes>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-import CssHeader from"./Header.module.css";
+import { NavLink } from "react-router-dom";
+import styles from"./Header.module.css";
 
-const Header = () => {
+const Header = (props) => {
     return (
-<header className={CssHeader.header}>
+<header className={styles.header}>
             <img src='https://i.pinimg.com/originals/e8/7a/fb/e87afb485ae35f89aceef01bc65ff1c0.png' />
+        <div className={styles.loginBlock} >
+            { props.isAuth ? props.login
+        : <NavLink to={'/login'} >Login</NavLink> }
+        </div>
         </header>
     )
 }
