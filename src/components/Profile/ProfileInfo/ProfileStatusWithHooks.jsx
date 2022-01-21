fc8292c29d23c3bc857333cf577ae629f4379b83
Alex Mercy <hambiru@yandex.ru>
2022-01-21 23:25:22 +0300

    84 lesson - hook useState

---
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useState } from "react";
+
+const ProfileStatusWithHooks = (props) => {
+    let [editMode, setEditMode] = useState(false);
+    let [status, setStatus] = useState(props.status);
+    
+  
+    const activateEditMode = () => {
+      setEditMode(true);
+        }
+        
+    const deActivateEditMode = () => {
+        setEditMode(false);
+        props.updateUserStatus(status);
+    }
+
+    const onStatusChange = (e) => {
+            setStatus(e.currentTarget.value)
+    }
+
+        return (
+            <div>
+                {!editMode &&
+                    <div>
+                        <span onDoubleClick={activateEditMode} >{props.status || "-----"}</span>
+                    </div>
+                }
+                {editMode &&
+                    <div>
+                        <input onChange={onStatusChange} autoFocus={true} 
+                        onBlur={deActivateEditMode} value={status} />
+                    </div>
+                }
+            </div>
+        )
+}
+
+
+export default ProfileStatusWithHooks;
