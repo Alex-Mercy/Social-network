a61b331914481b4dd080b59e6a9e828087b2f291
Alex Mercy <hambiru@yandex.ru>
2022-01-28 20:02:12 +0300

    94 lesson - React.lazy, React.Suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import React, { Component } from "react";
+import React, { Component, Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 import './App.css';
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Navbar from "./components/Navbar/Navbar";
-import ProfileContainer from "./components/Profile/ProfileContainer";
+// import ProfileContainer from "./components/Profile/ProfileContainer";
+// import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import Login from "./components/Login/Login";
 import { connect } from "react-redux";
 import {initializeApp} from "./redux/appReducer";
 import Preloader from "./components/common/Preloader/Preloader";
 
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
 
 
 class  App extends Component  {
@@ -34,15 +36,21 @@ class  App extends Component  {
                 <HeaderContainer />
                 <Navbar />
                 <div className="app-wrapper__content">
+                <Suspense fallback={<Preloader />}>
                     <Routes>
                         <Route path="/dialogs/*" element={<DialogsContainer />} />
                         <Route path="/profile/*" element={<ProfileContainer />}  />
+                        </Routes>
+                        </Suspense>
+                        <Routes>
                         <Route path="/users" element={<UsersContainer />} />
                         <Route path="/news" element={<News />} />
                         <Route path="/music" element={<Music />} />
                         <Route path="/settings" element={<Settings />} />
                         <Route path="/login" element={<Login />} />
-                    </Routes>
+                        </Routes>
+                    
+                    
                 </div>
             </div>
         
